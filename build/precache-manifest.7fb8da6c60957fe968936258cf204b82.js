6822065ad1729495de9c09acd96e2573fd7fcafa
Ivan Hill <30849044+ivan-hill@users.noreply.github.com>
2021-06-27 08:57:10 +0200

    Add files via upload

---
+++ build/precache-manifest.7fb8da6c60957fe968936258cf204b82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60c196eec7dcef3729947812772dd55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2aa1928c78e673234cd",
+    "url": "/static/css/main.e16c2567.chunk.css"
+  },
+  {
+    "revision": "b03bd0623cd412316073",
+    "url": "/static/js/2.cf2383f2.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.cf2383f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2aa1928c78e673234cd",
+    "url": "/static/js/main.ed8cebbc.chunk.js"
+  },
+  {
+    "revision": "22d0d5a6ac47d8429de1",
+    "url": "/static/js/runtime-main.fb398fac.js"
+  },
+  {
+    "revision": "9c656f51e3d308901709905a2c507a5e",
+    "url": "/static/media/compressed-image.9c656f51.png"
+  },
+  {
+    "revision": "490d130716e0074a5f9117435b04b49d",
+    "url": "/static/media/registered_pharmacy_technician.490d1307.png"
+  }
+]);
